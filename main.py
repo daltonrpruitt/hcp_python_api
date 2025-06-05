1f036b745d4dcbed2ad7ef3bfc61efffce29ed43
Dalton P <58988139+daltonrpruitt@users.noreply.github.com>
2025-06-05 15:53:50 -0500

    Make the api key into a environment variable
    Kinda important to not be hardcoded in a git-tracked repo...

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import requests
-
+import os
 
 def main():
+    try:
+        HCP_API_KEY = os.environ['HCP_API_KEY']
+    except KeyError as e:
+        e.add_note("No 'HCP_API_KEY' environment variable found!")
+        e.add_note("Set environment variables via:")
+        e.add_note("   $env:HCP_API_KEY = 'XYZ'")
+        e.add_note("in Powershell or")
+        e.add_note("   export HCP_API_KEY='XYZ'")
+        e.add_note("in Bash.")
+        raise 
 
-    API_KEY = 'your_api_key_here'
+    assert(len(HCP_API_KEY) == 32, "API Key is wrong length!")
+    
     headers = {
-        'Authorization': f'Bearer {API_KEY}',
+        'Authorization': f'Bearer {HCP_API_KEY}',
         'Content-Type': 'application/json'
     }
 
